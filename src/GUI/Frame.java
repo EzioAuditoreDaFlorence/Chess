a32ab85163a00c279ca11a482eeb8d03c64c7898
TaiSmile <2001040183@s.hanu.edu.vn>
2021-12-07 10:27:09 +0700

    Add packages

---
+++ src/GUI/Frame.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+
+    Frame() {
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setTitle("Chess");
+    }
+}
