318aa81d1ec0f244e663abd2fca7afb2c2b7b9ab
EzioAuditoreDaFlorence <77148455+EzioAuditoreDaFlorence@users.noreply.github.com>
2021-12-07 13:20:18 +0700

    Update Rook.java

--- src/ChessPiece/Rook.java
+++ src/ChessPiece/Rook.java
@@ -6,4 +6,10 @@ public class Rook extends Piece{
     public Rook(int x, int y, boolean isWhite) {
         super(x, y, isWhite);
     }
+    
+    @Override
+    boolean canMove(int target_x,int y) {
+        if (target_x != this.x) return false;
+        return true;
+    }
 }
